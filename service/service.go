8d08907c484dea900fac8fecc06a8177cef87a22
bofei5675 <bz1030@nyu.edu>
2019-09-08 22:28:21 -0400

    update post

--- service/service.go
+++ service/service.go
@@ -1,16 +1,18 @@
 package service
 
 
-import(
-	"github.com/gin-gonic/gin"
+import (
+	_ "../handler"
 	"../model"
-	. "../handler"
 	"../pkg/errno"
+	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func AddUser(c *gin.Context){
 	var r model.User
 	// bind check Content-Type to select a binding automatically
+	log.Printf("Start adding user %s", r)
 	if err := c.Bind(&r); err != nil {
 		SendResponse(c, errno.ErrBind, nil)
 		return
@@ -19,12 +21,14 @@ func AddUser(c *gin.Context){
 		UserName: r.UserName,
 		Password: r.Password,
 	}
+	log.Printf("Parse user from request %s", u)
 	// Validate the data
 	if err := u.Validate(); err != nil {
 		SendResponse(c, errno.ErrValidation, nil)
 		return
 	}
 	// Insert the user to the db
+	log.Printf("Insert into DB")
 	if _, err := u.Create(); err != nil {
 		SendResponse(c, errno.ErrDatabase,nil)
 		return
@@ -35,6 +39,7 @@ func AddUser(c *gin.Context){
 
 func SelectUser(c *gin.Context){
 	name := c.Query("user_name")
+	log.Printf("Start search user %s",name)
 	if name == ""{
 		SendResponse(c, errno.ErrValidation, nil)
 		return
@@ -43,9 +48,11 @@ func SelectUser(c *gin.Context){
 	if err := user.SelectUserByName(name) ; err != nil {
 		SendResponse(c, errno.ErrUserNotFound, nil)
 	}
+	log.Printf("Finish Select.")
 	if err := user.Validate(); err != nil {
 		SendResponse(c, errno.ErrUserNotFound, nil)
 		return
 	}
+	log.Printf("Send back the user %s", user)
 	SendResponse(c, nil, user)
 }
